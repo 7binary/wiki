8eacc07d0e586df145978d821f91e2b46998ec1c
Zinoviev Artem <7binary@gmail.com>
2021-04-11 07:56:52 +0300

    Updated http wiki with v2

--- vanilla-js/yandex.js
+++ vanilla-js/yandex.js
@@ -86,20 +86,16 @@ function compress(list) {
   console.log(result, ranges);
 }
 
-// https://www.youtube.com/watch?v=GUNl1DZlrYs&t=1415s -- автотесты
 console.log('********************************');
+// определить очередность вывода консольки
 
 console.log('apple');
-
 setTimeout(() => console.log('pear'), 0);
-
 Promise.resolve('melon').then(res => console.log(res));
-
 new Promise((resolve, reject) => {
   console.log('orange');
   resolve('pineapple');
 }).then(res => console.log(res));
-
 console.log('lime');
 
 // apple orange lime melon pineapple pear
